a31845f4ceb3a5a83daccb84c175310cfd64cba5
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-02-20 12:29:41 +0300

    [#155352030] Add templates and routes

--- app/views.py
+++ app/views.py
@@ -8,3 +8,51 @@ from app import app
 def index():
     # return render_template("index.html")
     return 'Hello WeConnect!'
+
+@app.route('/user/register')
+def register():
+    """Route to user registration page"""
+
+    return render_template("user/register.html")
+
+@app.route('/user/login')
+def login():
+    """Route to user login page"""
+
+    return render_template("user/login.html")
+
+@app.route('/user/update')
+def update_user():
+    """Route to user update page"""
+
+    return render_template("user/update.html")
+
+@app.route('/business')
+def businesses():
+    """Route to businesses list page"""
+
+    return render_template("business/index.html")
+
+@app.route('/business/register')
+def register_business():
+    """Route to businesses list page"""
+
+    return render_template("business/register.html")
+
+@app.route('/business/show')
+def show_business():
+    """Route to businesses list page"""
+
+    return render_template("business/show.html")
+
+@app.route('/category')
+def categories():
+    """Route to categories list page"""
+
+    return render_template("category/index.html")
+
+@app.route('/location')
+def locations():
+    """Route to locations list page"""
+
+    return render_template("location/index.html")
