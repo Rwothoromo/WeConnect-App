c2b52deabbd0f73896d6a02d956da64cad50ffda
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-02-20 15:48:38 +0300

    [#155352042] Add category tests

--- app/models/weconnect.py
+++ app/models/weconnect.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash
 
 # local imports
 # from app.models.business import Business
-# from app.models.category import Category
+from app.models.category import Category
 # from app.models.location import Location
 # from app.models.review import Review
 from app.models.user import User
@@ -52,3 +52,95 @@ class WeConnect(object):
                 return self.users[username]
             return "Incorrect username and password combination!"
         return "This username does not exist! Please register!"
+# Begin categories
+
+    def create_category(self, username, name, description):
+        """Create Category.
+        Add an instance of the Category class to the categories dictionary
+        of unique names as keys, if username exists in users dictionary.
+
+        :param username:    A string: username of the active user
+        :param name:        A string: name of the category to edit
+        :param description: A string: Some details on the category
+        :return:            categories dictionary
+        """
+
+        if username in self.users.keys():
+            if not all(isinstance(i, str) for i in [name, description]):
+                raise TypeError("Input should be a string!")
+
+            if name not in self.categories:
+                category = Category(name, description)
+
+                # Update categories in WeConnect
+                self.categories[name] = category
+
+                # Update user categories
+                self.users[username].categories[name] = category
+
+                return self.categories
+            return "This category already exists!"
+        return "Username does not exist!"
+
+    def view_category(self, username, name):
+        """Display a Category
+        Display a Category if it exists in the categories dictionary,
+        and if username exists in users dictionary.
+
+        :param username: A string: username of the active user
+        :param name:     A string: name of the category to view
+        :return:         The value of key matching the category name
+        """
+        if username in self.users.keys():
+            if name in self.categories:
+                return self.categories[name]
+            return "Category does not exist!"
+        return "Username does not exist!"
+
+    def edit_category(self, username, name, description):
+        """Edit a Category
+        Edit a Category if username exists in users dictionary.
+
+        :param username:    A string: username of the active user
+        :param name:        A string: name of the category to edit
+        :param description: A string: Some details on the category
+        :return:            categories dictionary
+        """
+
+        # Update the description
+        if self.users[username].categories[name]:
+            if not all(isinstance(i, str) for i in [name, description]):
+                raise TypeError("Input should be a string!")
+
+            updated_category = Category(name, description)
+
+            # Update categories in WeConnect
+            self.categories[name] = updated_category
+
+            # Update user categories
+            self.users[username].categories[name] = updated_category
+
+            return self.categories
+        return "User did not create the category!"
+
+    def delete_category(self, username, name):
+        """Delete a Category
+        Delete a Category if username exists in users dictionary.
+
+        :param username: A string: username of the active user
+        :param name:     A string: name of the category to delete
+        :return:         categories dictionary
+        """
+
+        if self.users[username].categories[name]:
+            if not isinstance(name, str):
+                raise TypeError("Input should be a string!")
+
+            # Delete this category from WeConnect
+            del self.categories[name]
+
+            # Delete this category from the categories of this user
+            del self.users[username].categories[name]
+
+            return self.categories
+        return "User did not create the category!"
