ca100aa9b6af29498ede33a804b4d8287d03bb04
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-03-16 16:30:32 +0300

    [#155326354] Update manage.py return statement

--- manage.py
+++ manage.py
@@ -9,10 +9,9 @@ def test():
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
 
-    # if result.wasSuccessful():
-    #     return 0
-    # return 1
-    return result.wasSuccessful()
+    # if result.wasSuccessful() return 0 else 1
+    # return the opposite boolean
+    return not result.wasSuccessful()
 
 if __name__ == '__main__':
     manager.run()
