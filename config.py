25cc45ef8cb6e57660cdae9f0357046b75921d46
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-03-23 09:20:47 +0300

    [#155326354] Adjust SQLALCHEMY_DATABASE_URI config to clear travis error

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config(object):
     """
 
     SECRET_KEY = 'some value'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] if os.environ[
-        'DATABASE_URL'] else 'postgresql://db_user:password@localhost/test_weconnect_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:password@localhost/test_weconnect_db' if not os.environ[
+        'DATABASE_URL'] else os.environ['DATABASE_URL']
 
     TESTING = False
     DEBUG = False
